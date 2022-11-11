67035acf916ef8aba6a7a4febb8cddcdbf8a37c4
kayi0919 <hltw2013@gmail.com>
2022-11-11 17:26:09 +0800

    1111

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'BPage.dart';
+import 'CPage.dart';
+import 'DPage.dart';
+import 'EPage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = '409630398_image';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
       home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          backgroundColor: Colors.green,
+        ),
+        body: HomePage(),
       ),
     );
   }
 }
 
-/// stateless widget that the main application instantiates
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            const SizedBox(height: 30),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    final snackBar = SnackBar(
-                      content: const Text('上一頁'),
-                      action: SnackBarAction(
-                        label: '神魔之塔',
-                        onPressed: () => Fluttertoast.showToast(
-                            msg:'你按下上一頁',
-                            toastLength: Toast.LENGTH_LONG,
-                            gravity: ToastGravity.CENTER,
-                            backgroundColor: Colors.green,
-                            textColor: Colors.white,
-                            fontSize: 30.0),
-                      ),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  child: const Text('上一頁'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    final snackBar = SnackBar(
-                      content: const Text('下一頁'),
-                      action: SnackBarAction(
-                        label: '神魔之塔',
-                        onPressed: () => Fluttertoast.showToast(
-                            msg:'你按下下一頁',
-                            toastLength: Toast.LENGTH_LONG,
-                            gravity: ToastGravity.CENTER,
-                            backgroundColor: Colors.green,
-                            textColor: Colors.white,
-                            fontSize: 30.0),
-                      ),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  child: const Text('下一頁'),
-                ),
-              ],
-            )
-          ],
-        )
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: <Widget>[
+        TextButton(
+          child: const Icon(Icons.videogame_asset),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => const BPage())
+            );
+          },
+        ),
+        ElevatedButton(
+          child: const Icon(Icons.menu_book),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => CPage())
+            );
+          },
+        ),
+        OutlinedButton(
+          child: const Icon(Icons.image),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => DPage())
+            );
+          },
+        ),
+        TextButton(
+          child: const Icon(Icons.videogame_asset),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => EPage())
+            );
+          },
+        ),
+        const Expanded(
+          child: FittedBox(
+            child: FlutterLogo(),
+          ),
+        ),
+      ],
+
     );
+
   }
 }
